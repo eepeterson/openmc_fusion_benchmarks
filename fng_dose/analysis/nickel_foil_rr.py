15a5a5ec0ad7856ed4355545df3591df9325c440
Paul Romano <paul.k.romano@gmail.com>
2023-08-24 22:27:18 -0500

    Add Ni foil C/E plot

--- fng_dose/analysis/nickel_foil_rr.py
+++ fng_dose/analysis/nickel_foil_rr.py
@@ -1,11 +1,20 @@
 import argparse
 from math import pi
 
+from matplotlib.patches import Polygon
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import openmc
 from prettytable import PrettyTable
 from uncertainties import ufloat
 
+mpl.rcParams['axes.labelsize'] = 'large'
+mpl.rcParams['axes.axisbelow'] = True
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['pdf.use14corefonts'] = True
+mpl.rcParams['savefig.bbox'] = 'tight'
+mpl.rcParams['font.size'] = 12.0
 
 parser = argparse.ArgumentParser()
 parser.add_argument('statepoint')
@@ -39,3 +48,21 @@ for i, (exp, mean, std) in enumerate(zip(experiment_n2n_rate, n2n_mean, n2n_std)
     table.add_row([i+1, f'{exp:.2e}', ufloat(mean, std)])
 print("\n(n,2n) reaction rate")
 print(table)
+
+ce_n2n = n2n_mean/experiment_n2n_rate
+ce_n2n_unc = n2n_std/experiment_n2n_rate
+ce_np = np_mean/experiment_np_rate
+ce_np_unc = np_std/experiment_np_rate
+
+fig, ax = plt.subplots()
+ax.errorbar([1, 2, 3, 4, 5, 6], ce_np, ce_np_unc, ls='none', marker='x', label='(n,p)')
+ax.errorbar([1, 2, 3, 4, 5, 6], ce_n2n, ce_n2n_unc, ls='none', marker='o', label='(n,2n)')
+poly = Polygon([(0, 0.95), (0, 1.05), (7, 1.05), (7, 0.95)], facecolor='gray', edgecolor=None, alpha=0.2)
+ax.add_patch(poly)
+ax.legend(loc='upper right', framealpha=1.0)
+ax.set_xlim(0.5, 6.5)
+ax.set_ylim(0.75, 1.15)
+ax.set_xlabel('Foil')
+ax.set_ylabel('C/E')
+ax.grid(True, axis='y')
+plt.savefig('ni_foil_ce.pdf')
