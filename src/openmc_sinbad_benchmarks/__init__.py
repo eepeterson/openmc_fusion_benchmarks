cc51573bb2f67b01591c46f717eccb4484225111
Stefano Segantin <segantin@psfc.mit.edu>
2023-10-03 14:37:34 -0400

    +database file and classes in within

--- src/openmc_sinbad_benchmarks/__init__.py
+++ src/openmc_sinbad_benchmarks/__init__.py
@@ -1,4 +1,5 @@
 from openmc_sinbad_benchmarks import from_irdff
 from openmc_sinbad_benchmarks import neutron_sources
+from openmc_sinbad_benchmarks import database
 
 __version__ = '0.1.0-dev'
--- src/openmc_sinbad_benchmarks/database.py
+++ src/openmc_sinbad_benchmarks/database.py
@@ -1,4 +1,21 @@
 import h5py
+import pandas as pd
+
+
+class TallyFromDatabase:
+    def __init__(self, filename: str, tally_name: str):
+        self.filename = filename
+        self.tally_name = tally_name
+
+        with h5py.File(self.filename) as f:
+            self.tally = f[tally_name+'/table']
+
+    def get_pandas_dataframe(self):
+        return pd.DataFrame(self.tally[()])
+
+    @property
+    def get_xaxis_label(self):
+        return self.tally.attrs['x_axis']
 
 
 class ResultFromDatabase:
@@ -7,12 +24,46 @@ class ResultFromDatabase:
 
         self.filename = filename
 
-    def get_tally(self, tally_name: str):
+    def list_tallies(self):
         with h5py.File(self.filename) as f:
-            return f[tally_name+'/table'][()]
+            print(f.keys())
 
-    def get_columns(self, tally_name: str):
-        pass
+    def get_tally(self, tally_name: str):
+        return TallyFromDatabase(self.filename, tally_name)
 
-    def get_xaxis_label(self, tally_name: str):
-        pass
+    @property
+    def literature_info(self):
+        with h5py.File(self.filename) as f:
+            return f.attrs['literature']
+
+    @property
+    def when(self):
+        with h5py.File(self.filename) as f:
+            return f.attrs['when']
+
+    @property
+    def where(self):
+        with h5py.File(self.filename) as f:
+            return f.attrs['where']
+
+    @property
+    def code_version(self):
+        with h5py.File(self.filename) as f:
+            return f.attrs['code_version']
+
+    @property
+    def xs_library(self):
+        with h5py.File(self.filename) as f:
+            return f.attrs['xs_library']
+
+    @property
+    def print_code_info(self):
+        print(f'Code version:{self.code_version} \n',
+              f'XS library: {self.xs_library} \n')
+
+    def print_all_info(self):
+        print(f'When: {self.when} \n',
+              f'Where: {self.where} \n',
+              f'Code version:{self.code_version} \n',
+              f'XS library: {self.xs_library} \n'
+              f'Literature: {self.literature_info} \n')
