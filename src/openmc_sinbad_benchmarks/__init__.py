de6037cec8911c260027058f09f656094d28efa2
Stefano Segantin <segantin@psfc.mit.edu>
2023-10-13 11:32:14 -0400

    + postprocessing_v2 to try things

--- src/openmc_sinbad_benchmarks/__init__.py
+++ src/openmc_sinbad_benchmarks/__init__.py
@@ -1,5 +1,5 @@
 from openmc_sinbad_benchmarks import from_irdff
 from openmc_sinbad_benchmarks import neutron_sources
-from openmc_sinbad_benchmarks import database
+from openmc_sinbad_benchmarks.database import *
 
 __version__ = '0.1.0-dev'
