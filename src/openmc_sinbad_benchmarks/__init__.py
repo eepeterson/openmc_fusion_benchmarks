2c99065f1f037f66c3342312eb4a63c23eb44346
Stefano Segantin <segantin@psfc.mit.edu>
2024-01-02 10:15:29 -0500

    new pkg name openmc_fusion_benchmarks

--- src/openmc_sinbad_benchmarks/__init__.py
+++
@@ -1,6 +0,0 @@
-from openmc_sinbad_benchmarks import from_irdff
-import openmc_sinbad_benchmarks.neutron_sources
-from openmc_sinbad_benchmarks.read_results import *
-from openmc_sinbad_benchmarks.visualize import *
-
-__version__ = '0.1.0-dev'
