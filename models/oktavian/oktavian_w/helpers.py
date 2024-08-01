3a477e04521fa7caaac770562edf0cdef9681af8
davidepettinari <davide.pettinari@polito.it>
2024-08-01 17:22:48 +0200

    update

--- models/oktavian/oktavian_w/helpers.py
+++ models/oktavian/oktavian_w/helpers.py
@@ -1,63 +1,7 @@
 import numpy as np
 import pandas as pd
+import math
 
-def process_n_tally(tally_dataframe):
-    # Extract the "energy low [eV]" column from the dataframe and convert to float array
-    nenergies = tally_dataframe["energy low [eV]"].astype(float).values
-    
-    # Initialize an array of energy values, starting with a small "zero" energy value
-    nergs = [1e-10]
-    # Append the extracted energy values to the list
-    nergs.extend(nenergies.tolist())
-    # Convert the list to a numpy array for numerical operations
-    nergs = np.array(nergs)
-    
-    # Compute lethargy
-    # Calculate the corrected mean values by dividing the original mean values
-    # by the logarithm of the ratio of consecutive energy intervals
-    mean_corrected = tally_dataframe["mean"].values / np.log(nergs[1:] / nergs[:-1])
-    
-    # Calculate the corrected standard deviation in a similar manner
-    stddev_corrected = tally_dataframe['std. dev.'].values / np.log(nergs[1:] / nergs[:-1])
-    
-    # Return the corrected mean and standard deviation arrays
-    return mean_corrected, stddev_corrected
-
-def process_g_tally(tally_dataframe):
-    # Extract the "energy low [eV]" column, convert to float and then to MeV
-    genergies = tally_dataframe["energy low [eV]"].astype(float).values / 1e6
-    
-    # Initialize an array of energy values, starting with a small "zero" energy value
-    gergs = [1e-10]
-    # Append the extracted energy values to the list
-    gergs.extend(genergies.tolist())
-    # Convert the list to a numpy array for numerical operations
-    gergs = np.array(gergs)
-    
-    # Compute per unit energy
-    # Calculate the corrected mean values by dividing the original mean values
-    # by the difference of consecutive energy intervals
-    mean_corrected = tally_dataframe["mean"].values / (gergs[1:] - gergs[:-1])
-    
-    # Calculate the corrected standard deviation in a similar manner
-    stddev_corrected = tally_dataframe["std. dev."].values / (gergs[1:] - gergs[:-1])
-    
-    # Return the corrected mean and standard deviation arrays
-    return mean_corrected, stddev_corrected
-
-def postprocess_openmc_spectra(openmc_df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Post-process the OpenMC spectra dataframe by removing certain columns.
-
-    Parameters:
-    openmc_df (pd.DataFrame): The input dataframe from OpenMC results.
-
-    Returns:
-    pd.DataFrame: The dataframe with specified columns removed.
-    """
-    # Remove the specified columns if they exist
-    columns_to_remove = ['surface', 'particle', 'nuclide', 'score']
-    for column in columns_to_remove:
-        if column in openmc_df.columns:
-            del openmc_df[column]
-    return openmc_df
+# sphere info
+r = 19.95
+surface = 4 * math.pi * r**2
--- models/oktavian/oktavian_w/run_and_store.py
+++ models/oktavian/oktavian_w/run_and_store.py
@@ -45,57 +45,22 @@ def main():
     # read statepoint file
     openmc_file = ofb.ResultsFromOpenmc('statepoint.100.h5', 'results')
 
-    # generate openmc hdf file
-    filename = ofb.build_hdf_filename(
-        'openmc', openmc_file.get_openmc_version, args.xslib)
+    xaxis_name = 'energy low [eV]'
+    openmc_file.tally_to_hdf(tally_name=f'nspectrum',
+                             normalize_over=helpers.surface,
+                             xs_library=args.xslib,
+                             xaxis_name=xaxis_name,
+                             path_to_database='results_database',
+                             when=args.when,
+                             where=args.where)
     
-    # store nuclear spectra results
-    # # rearrange
-    xaxis_name = 'energy low [eV]'
-    n_tally_name = 'nspectrum'
-    n_openmc_df = openmc_file.get_tally_dataframe(n_tally_name)
-    n_tally_df = helpers.postprocess_openmc_spectra(n_openmc_df)
-
-    path_to_file = Path('results_database') / filename
-
-    # write the tally in the hdf file
-    n_tally_df.to_hdf(path_to_file, n_tally_name, mode='a',
-                    format='table', data_columns=True, index=False)
-    code_version = 'openmc-' + \
-        '.'.join(map(str, openmc_file.get_openmc_version))
-
-    # write attributes to the hdf file
-    with h5py.File(path_to_file, 'a') as f:
-        f[n_tally_name + '/table'].attrs['x_axis'] = xaxis_name
-        f.attrs['code_version'] = code_version
-        f.attrs['xs_library'] = args.xslib.strip().replace(' ', '')
-        f.attrs['batches'] = openmc_file.get_batches
-        f.attrs['particles_per_batch'] = openmc_file.get_particles_per_batch
-        f.attrs['when'] = args.when
-        f.attrs['where'] = args.where
-
-    xaxis_name = 'energy low [eV]'
-    g_tally_name = 'gspectrum'
-    g_openmc_df = openmc_file.get_tally_dataframe(g_tally_name)
-    g_tally_df = helpers.postprocess_openmc_spectra(g_openmc_df)
-
-    path_to_file = Path('results_database') / filename
-
-    # write the tally in the hdf file
-    g_tally_df.to_hdf(path_to_file, g_tally_name, mode='a',
-                    format='table', data_columns=True, index=False)
-    code_version = 'openmc-' + \
-        '.'.join(map(str, openmc_file.get_openmc_version))
-
-    # write attributes to the hdf file
-    with h5py.File(path_to_file, 'a') as f:
-        f[g_tally_name + '/table'].attrs['x_axis'] = xaxis_name
-        f.attrs['code_version'] = code_version
-        f.attrs['xs_library'] = args.xslib.strip().replace(' ', '')
-        f.attrs['batches'] = openmc_file.get_batches
-        f.attrs['particles_per_batch'] = openmc_file.get_particles_per_batch
-        f.attrs['when'] = args.when
-        f.attrs['where'] = args.where
+    openmc_file.tally_to_hdf(tally_name=f'gspectrum',
+                             normalize_over=helpers.surface,
+                             xs_library=args.xslib,
+                             xaxis_name=xaxis_name,
+                             path_to_database='results_database',
+                             when=args.when,
+                             where=args.where)
         
 if __name__ == "__main__":
     main()
