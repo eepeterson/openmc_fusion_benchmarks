5305442e0097380090adf0bc6b75e6dd4236d7b7
davidepettinari <davide.pettinari@polito.it>
2024-10-01 04:04:58 +0200

    update geometry of oktavian_cu, oktavian_lif, oktavian_mn

--- models/oktavian/oktavian_mn/openmc_model.py
+++ models/oktavian/oktavian_mn/openmc_model.py
@@ -75,20 +75,19 @@ def main():
     # Define Geometry
 
     # surfaces
-    surf_3 = openmc.Sphere(surface_id=3, x0=0.0, y0=0.0, z0=0.0, r=10.0)
-    surf_8 = openmc.XPlane(surface_id=8, x0=8.32)
-    surf_1 = openmc.XCylinder(surface_id=1, y0=0.0, z0=0.0, r=5.55)
-    surf_6 = openmc.Sphere(surface_id=6, x0=0.0, y0=0.0, z0=0.0, r=19.95)
-    surf_4 = openmc.Sphere(surface_id=4, x0=0.0, y0=0.0, z0=0.0, r=10.2)
-    surf_2 = openmc.XCylinder(surface_id=2, y0=0.0, z0=0.0, r=5.75)
-    surf_5 = openmc.Sphere(surface_id=5, x0=0.0, y0=0.0, z0=0.0, r=19.75)
+    surf_8 = openmc.XPlane(surface_id=8, x0=-2.2)
+    surf_1 = openmc.XCylinder(surface_id=1, y0=0.0, z0=0.0, r=2.55)
+    surf_6 = openmc.Sphere(surface_id=6, x0=0.0, y0=0.0, z0=0.0, r=30.5)
+    surf_9 = openmc.XPlane(surface_id=9, x0=-2.5)
+    surf_2 = openmc.XCylinder(surface_id=2, y0=0.0, z0=0.0, r=2.85)
+    surf_5 = openmc.Sphere(surface_id=5, x0=0.0, y0=0.0, z0=0.0, r=30.0)
     surf_7 = openmc.Sphere(surface_id=7, x0=0.0, y0=0.0, z0=0.0, r=100.0, boundary_type='vacuum')
 
     # regions
-    region_1 = ((-surf_3 & -surf_8) | (+surf_8 & -surf_1 & -surf_6))
-    region_2 = ((+surf_3 & -surf_4 & -surf_8) | (+surf_8 & +surf_1 & -surf_2 & -surf_6))
-    region_3 = ((+surf_4 & -surf_5 & -surf_8) | (+surf_8 & +surf_2 & -surf_5))
-    region_4 = ((+surf_5 & -surf_6 & -surf_8) | (+surf_8 & +surf_2 & +surf_5 & -surf_6))
+    region_1 = (+surf_8 & -surf_1 & -surf_6)
+    region_2 = ((-surf_8 & +surf_9 & -surf_2) | (+surf_8 & +surf_1 & -surf_2 & -surf_5))
+    region_3 = ((-surf_5 & -surf_9) | (+surf_9 & +surf_2 & -surf_5))
+    region_4 = ((+surf_5 & -surf_6 & -surf_8) | (+surf_8 & +surf_1 & +surf_5 & -surf_6))
     region_5 = (+surf_6 & -surf_7)
 
     # cells
