846eaf70d5642370d456c7374654a0107e759c1e
Stefano Segantin <segantin@psfc.mit.edu>
2025-02-26 11:50:44 -0500

    small change in init

--- src/openmc_fusion_benchmarks/__init__.py
+++ src/openmc_fusion_benchmarks/__init__.py
@@ -3,7 +3,7 @@ import openmc_fusion_benchmarks.neutron_sources
 from openmc_fusion_benchmarks.read_results import *
 from openmc_fusion_benchmarks.visualize import *
 from openmc_fusion_benchmarks.utils import *
+from openmc_fusion_benchmarks.benchmark import *
 
 # new
-from openmc_fusion_benchmarks.benchmark import *
 from openmc_fusion_benchmarks.statepoint import *
