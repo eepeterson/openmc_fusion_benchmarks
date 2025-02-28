8e7720a49566e109bb4aec0ea0a612c4c350bd6b
Stefano Segantin <segantin@psfc.mit.edu>
2025-02-28 11:20:36 -0500

    fixed import in init file

--- src/openmc_fusion_benchmarks/__init__.py
+++ src/openmc_fusion_benchmarks/__init__.py
@@ -1,4 +1,4 @@
-from openmc_fusion_benchmarks import from_irdff
+from openmc_fusion_benchmarks import irdff
 import openmc_fusion_benchmarks.neutron_sources
 from openmc_fusion_benchmarks.read_results import *
 from openmc_fusion_benchmarks.visualize import *
