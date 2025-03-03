f6f988316ee2780a06b9144b46a7e4d91afc30fd
Stefano Segantin <segantin@psfc.mit.edu>
2025-03-03 14:53:20 -0500

    + download_geometry()

---
+++ src/openmc_fusion_benchmarks/download.py
@@ -0,0 +1,28 @@
+import json
+import gdown
+import importlib
+
+
+LIB_PATH = importlib.resources.files(
+    "openmc_fusion_benchmarks.lib")
+
+
+def download_geometry(benchmark_name: str, file_format: str, run_option: str = None):
+
+    filepath = LIB_PATH / "cad_geometries.json"
+    with open(filepath, "r") as f:
+        data = json.load(f)
+
+        # Your Google Drive file link
+    if run_option is not None:
+        url = data[benchmark_name][run_option][file_format]
+    else:
+        url = data[benchmark_name][file_format]
+
+    # Extract the file ID from the URL
+    file_id = url.split("/d/")[1].split("/")[0]
+    download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+
+    # Download the file
+    output = f"{benchmark_name}_{run_option}.{file_format}"
+    gdown.download(download_url, output=output, quiet=False)
