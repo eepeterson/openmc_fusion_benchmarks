271563d7851a0428a9fd5adc3dcea1a0f476efbc
Stefano Segantin <segantin@psfc.mit.edu>
2025-03-04 08:34:33 -0500

    + cwd arg in download_geometry

--- src/openmc_fusion_benchmarks/cloud_interface.py
+++ src/openmc_fusion_benchmarks/cloud_interface.py
@@ -7,7 +7,7 @@ LIB_PATH = importlib.resources.files(
     "openmc_fusion_benchmarks.lib")
 
 
-def download_geometry(benchmark_name: str, file_format: str, run_option: str = None):
+def download_geometry(benchmark_name: str, file_format: str, run_option: str = None, cwd: str = None):
 
     filepath = LIB_PATH / "dagmc_geometries.json"
     with open(filepath, "r") as f:
@@ -23,5 +23,10 @@ def download_geometry(benchmark_name: str, file_format: str, run_option: str = N
     file_id = url.split("/d/")[1].split("/")[0]
     download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
 
+    # make sure cwd is identified as directory:
+    if cwd is not None:
+        if not cwd.endswith("/"):
+            cwd += "/"
+
     # Download the file
-    gdown.download(download_url, quiet=False, use_cookies=False)
+    gdown.download(download_url, output=cwd, quiet=False, use_cookies=False)
